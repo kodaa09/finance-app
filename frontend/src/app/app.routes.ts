e98003fbb107d7cb70cd9f2f2493acdee46c7a1b
kodaa <je.elbaze@gmail.com>
2025-01-02 17:00:18 -0500

    add front routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'overview',
+    loadComponent: () =>
+      import('./pages/overview/overview.component').then(
+        (m) => m.OverviewComponent
+      ),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./pages/transactions/transactions.component').then(
+        (m) => m.TransactionsComponent
+      ),
+  },
+  {
+    path: 'budgets',
+    loadComponent: () =>
+      import('./pages/budgets/budgets.component').then(
+        (m) => m.BudgetsComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
